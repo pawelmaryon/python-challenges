41b239f023582c992e424a9b58865d115dadbe51
P Maryon <maryonpawel@gmail.com>
2022-05-24 16:28:21 +0100

    dynamic routes created, int and str types of url created as well

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path("january", views.january),
-path("february", views.february)
+# path("january", views.january),
+# path("february", views.february),
+path("<int:month>", views.monthly_challenges_by_number),
+path("<str:month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def january(request):
@@ -7,3 +7,20 @@ def january(request):
 
 def february(request):
   return HttpResponse("Walk for at least 20 minutes every day!")
+
+# def march(request):
+#   return HttpResponse("Code Django every day!")
+def monthly_challenges_by_number(request, month):
+  return HttpResponse(month)
+
+def monthly_challenge(request, month):
+  challenge_text = None
+  if month == "january":
+    challenge_text = "Eat meat for entire month"
+  elif month == "february":
+    challenge_text = "Walk for at least 20 minutes every day!"
+  elif month == "march":
+    challenge_text = "Code Django every day!"
+  else:
+    return HttpResponseNotFound("This month is not supported!")
+  return HttpResponse(challenge_text)
